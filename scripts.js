27599439ae1967be65088f6881ce3c2ef371c3d4
HabieruSenso <javier.guerra@live.u-tad.com>
2022-05-24 18:18:28 +0200

    Javi Promociones

--- scripts.js
+++ scripts.js
@@ -1,38 +1,38 @@
 window.addEventListener('scroll', function(e) {
     console.log(window.scrollY);
-  const arrow = document.querySelector('.arrow-up');
-  if(window.scrollY > 100) {
-      arrow.classList.add('visible');
-  } else {
-      arrow.classList.remove('visible');
-  }
+    const arrow = document.querySelector('.arrow-up');
+    if (window.scrollY > 100) {
+        arrow.classList.add('visible');
+    } else {
+        arrow.classList.remove('visible');
+    }
 });
 
-  function scrollUp() {
-      window.scroll({
-          top: 0,
-          behavior: 'smooth'
-      });
-  }
+function scrollUp() {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
 
-  function openSidebar() {
-      document.querySelector('.sidebar').classList.add('open');
-  }
+function openSidebar() {
+    document.querySelector('.sidebar').classList.add('open');
+}
 
-  function openDropDown() {
-      var dropdownContent = document.querySelector('.dropdown');
-      var svgIcon =  document.querySelector('.flecha-abajo');
-      var isOpen = dropdownContent.classList.contains('open');
+function openDropDown() {
+    var dropdownContent = document.querySelector('.dropdown');
+    var svgIcon = document.querySelector('.flecha-abajo');
+    var isOpen = dropdownContent.classList.contains('open');
 
-      if (isOpen) {
-          dropdownContent.classList.remove('open');
-          svgIcon.classList.remove('open');
-      } else {
-          dropdownContent.classList.add('open');
-          svgIcon.classList.add('open');
-      }
-  }
+    if (isOpen) {
+        dropdownContent.classList.remove('open');
+        svgIcon.classList.remove('open');
+    } else {
+        dropdownContent.classList.add('open');
+        svgIcon.classList.add('open');
+    }
+}
 
-  function closeSidebar() {
-      document.querySelector('.sidebar').classList.remove('open');
-  }
+function closeSidebar() {
+    document.querySelector('.sidebar').classList.remove('open');
+}
