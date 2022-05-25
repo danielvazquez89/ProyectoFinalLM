a81d341d99805de23887bd69affa3bd23b617302
Daniel Vázquez Pérez <danielvazquezperez5@gmail.com>
2022-05-25 09:18:07 +0200

    commit-base2

--- scripts.js
+++ scripts.js
@@ -1,38 +1,62 @@
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', function (e) {
     console.log(window.scrollY);
-  const arrow = document.querySelector('.arrow-up');
-  if(window.scrollY > 100) {
-      arrow.classList.add('visible');
-  } else {
-      arrow.classList.remove('visible');
-  }
+    const arrow = document.querySelector('.arrow-up');
+    if (window.scrollY > 150) {
+        arrow.classList.add('visible');
+    } else {
+        arrow.classList.remove('visible');
+    }
 });
 
-  function scrollUp() {
-      window.scroll({
-          top: 0,
-          behavior: 'smooth'
-      });
-  }
+function scrollUp() {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
 
-  function openSidebar() {
-      document.querySelector('.sidebar').classList.add('open');
-  }
+function openSidebar() {
+    document.querySelector('.sidebar').classList.add('open');
+}
 
-  function openDropDown() {
-      var dropdownContent = document.querySelector('.dropdown');
-      var svgIcon =  document.querySelector('.flecha-abajo');
-      var isOpen = dropdownContent.classList.contains('open');
+function openDropDown() {
+    var dropdownContent = document.querySelector('.dropdown');
+    var svgIcon = document.querySelector('.flecha-abajo');
+    var isOpen = dropdownContent.classList.contains('open');
 
-      if (isOpen) {
-          dropdownContent.classList.remove('open');
-          svgIcon.classList.remove('open');
-      } else {
-          dropdownContent.classList.add('open');
-          svgIcon.classList.add('open');
-      }
-  }
+    if (isOpen) {
+        dropdownContent.classList.remove('open');
+        svgIcon.classList.remove('open');
+    } else {
+        dropdownContent.classList.add('open');
+        svgIcon.classList.add('open');
+    }
+}
 
-  function closeSidebar() {
-      document.querySelector('.sidebar').classList.remove('open');
-  }
+function closeSidebar() {
+    document.querySelector('.sidebar').classList.remove('open');
+}
+function viewFullScreen() {
+    var imag = document.querySelector('.modal-image');
+    openFullscreen(imag);
+}
+
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+function hideModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('show');
+}
+function showModal(imageSrc, title, description) {
+    const modal = document.querySelector('.modal');
+    var string = `<img src="${imageSrc}" class="modal-image"><button onclick="hideModal()" class="cross-button">&#10006;</button><div class="modal-footer"><h3 class="modal-title">${title}</h3><div class="modal-description">${description}</div><button class="hideButton" onclick="hideModal()">Cerrar</button><button onclick="viewFullScreen()">View Full Screen</button></div>`
+    document.querySelector('.modal-content').innerHTML = string;
+    modal.classList.add('show');
+}
