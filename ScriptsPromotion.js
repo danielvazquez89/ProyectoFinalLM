d3c226eeb6f06c792668c886aa502ebc5d27cfaa
Daniel Vázquez Pérez <danielvazquezperez5@gmail.com>
2022-06-01 10:31:47 +0200

    Index-Nuevo

--- ScriptsPromotion.js
+++
@@ -1,9 +0,0 @@
-function openSidebar() {
-    console.log('sidebar');
-    document.querySelector('.sidebar').classList.add('open');
-}
-
-function closeSidebar() {
-    console.log('sidebar');
-    document.querySelector('.sidebar').classList.remove('open');
-}
